76621c88ae35e59dafe56e6aea231e98950e713a
Shymah2002 <Sitiintern@gmail.com>
2025-06-07 01:36:05 +0800

    Some common info

--- app/src/main/java/com/example/gaitguardian/ui/theme/Color.kt
+++ app/src/main/java/com/example/gaitguardian/ui/theme/Color.kt
@@ -1,6 +1,9 @@
 package com.example.gaitguardian.ui.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +12,34 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Colors
+val bgColor = Color(0xFFFFF6DD)
+val DefaultColor = Color.Black
+val cardBackgroundColor = Color.White
+val buttonBackgroundColor = Color(0xFFFFC279)
+val severityBoxColor = Color(0xFFFFD966)
+val progressGreen = Color(0xFF57B660)
+val progressYellow = Color(0xFFF3B400)
+val progressRed = Color(0xFFE23C3C)
+val progressMarkerColor = Color.White
+
+// Font sizes
+val languageFontSize = 20.sp
+val Heading1  = 25.sp // suitable for emphasis, buttons
+val Heading2 = 20.sp
+val subheading1 = 18.sp
+val body = 15.sp
+
+// Sizes & padding
+val spacerLarge = 30.dp
+val screenPadding = 16.dp
+val cardPadding = 25.dp
+val boxPadding = 12.dp
+val boxShape = RoundedCornerShape(8.dp)
+val progressBarHeight = 24.dp
+val progressMarkerWidth = 2.dp
+val progressMarkerOffset = 210.dp
+val homeIconSize = 50.dp
+val homeIconPadding = 8.dp
+val ButtonActive = Color(0xFFFFB703)
